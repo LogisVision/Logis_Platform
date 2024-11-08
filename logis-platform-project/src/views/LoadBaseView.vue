66c0d156d565614ca4bc90b3e61f5adb939df0f5
itdice <itdiceofficial@gmail.com>
2024-11-08 16:25:03 +0900

    Load Base View의 선반 표기 정보 구현

--- logis-platform-project/src/views/LoadBaseView.vue
+++ logis-platform-project/src/views/LoadBaseView.vue
@@ -9,7 +9,51 @@ import headerBox from '@/components/heaaderBox.vue';
       <header-box />
     </header>
     <main class="main-layout">
-      <h6>This is Load Base pages.</h6>
+
+      <div class="container mt-3">
+        <div class="row">
+          <div class="col-4 mb-4" v-for="i in 6" :key="i">
+            <div class="card load-card-box">
+              <div class="card-body">
+                <!-- Card Header -->
+                <div class="load-card-content pb-2">
+                  <div class="load-id">#00000000</div>
+                  <div class="load-label">A / 0</div>
+                </div>
+
+                <!-- Item Image -->
+                <div class="text-center mb-2">
+                  <img src="@/assets/images/emptyBox.png" alt="Item Image"
+                       class="img-fluid load-item-image">
+                </div>
+
+                <!-- Color Info -->
+                <div class="text-center load-color-info mb-2 ">None</div>
+                <!-- Load State -->
+                <div class="text-center load-state mb-3">
+                  Empty Space
+                </div>
+
+                <!-- Options -->
+                <div class="row mt-2 justify-content-center">
+                  <div class="col-8">
+                    <select class="form-select" id="itemSelection">
+                      <option value="" selected>Select an item</option>
+                      <option value="item1">Item 1</option>
+                      <option value="item2">Item 2</option>
+                      <option value="item3">Item 3</option>
+                    </select>
+                  </div>
+                  <div class="col-4 align-self-end">
+                    <button class="btn btn-secondary w-100 text-truncate">Decide</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </main>
     <footer class="footer-layout">
       <copyright-box />
@@ -18,5 +62,48 @@ import headerBox from '@/components/heaaderBox.vue';
 </template>
 
 <style scoped>
+.load-card-box {
+  background-color: var(--black-secondary-background); border-radius: 20px;
+}
 
+.load-card-content {
+  display: flex;
+  justify-content: space-between;
+}
+
+.load-id {
+  font-size: 1.5rem;
+  font-weight: 600;
+}
+
+.load-label {
+  color: var(--white-30-font);
+  font-size: 1.5rem;
+  font-weight: 600;
+}
+
+.load-item-image {
+  width: 40%;
+  max-width: 200px;
+
+  border-radius: 20px;
+}
+
+.load-state {
+  font-size: 1.2rem;
+  font-weight: 600;
+}
+
+.load-color-info {
+  max-width: 50%;
+
+  background-color: #000000;
+  border: 2px solid var(--white-background);
+  border-radius: 15px;
+  margin: 0 auto;
+
+  color: var(--white-font);
+  font-size: 1rem;
+  font-weight: 700;
+}
 </style>
