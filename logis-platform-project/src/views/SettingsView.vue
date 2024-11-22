916b4fe4884ae06c52d91d3b48181ad997e3fb1c
itdice <itdiceofficial@gmail.com>
2024-11-23 01:37:03 +0900

    설정 페이지 UI 추가

--- logis-platform-project/src/views/SettingsView.vue
+++ logis-platform-project/src/views/SettingsView.vue
@@ -9,7 +9,12 @@ import headerBox from '@/components/heaaderBox.vue';
       <header-box />
     </header>
     <main class="main-layout">
-      <h6>This is Settings pages.</h6>
+      <div class="page-container">
+        <span class="ps-2 pe-2 material-symbols-sharp const-icon">
+          construction
+        </span>
+        <div class="const-icon-code">INOP</div>
+      </div>
     </main>
     <footer class="footer-layout">
       <copyright-box />
@@ -18,5 +23,26 @@ import headerBox from '@/components/heaaderBox.vue';
 </template>
 
 <style scoped>
+.page-container {
+  width: 100%;
 
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  padding: 22vh 0;
+}
+
+.const-icon-code {
+  font-size: 4rem;
+  font-weight: 700;
+  color: var(--white-font);
+}
+
+.const-icon {
+  margin: 3px 0 0 -1px;
+  font-size: 8rem;
+  color: var(--white-font);
+}
 </style>
